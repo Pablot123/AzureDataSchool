8707ac09f7c8cd4f3ec093b816d719d98ff20c21
Pablo Andrés Tamayo Flórez <pablo.tamayo@DATALYTICS.local>
2023-03-09 17:02:24 -0500

    using mlflow

--- areaTrabajoSDK.py
+++
@@ -1,8 +0,0 @@
-import mlflow
-
-experiment_name = 'exp-prueba'
-mlflow.set_experiment(experiment_name)
-with mlflow.start_run(run_name="run-name") as run:
-    #row_columns = len(df.columns)
-    mlflow.log_metric('columnas', 1)
-    
