989356f55e2bafeef39edb990c3f4584d01ea6c8
Pablo Andrés Tamayo Flórez <pablo.tamayo@DATALYTICS.local>
2023-03-09 08:19:02 -0500

    creating pruebas.ipynb

--- areaTrabajoSDK.py
+++ areaTrabajoSDK.py
@@ -1,3 +1,8 @@
-from azureml.core import Workspace
+import mlflow
 
-ws = Workspace.from_config()
+experiment_name = 'exp-prueba'
+mlflow.set_experiment(experiment_name)
+with mlflow.start_run(run_name="run-name") as run:
+    #row_columns = len(df.columns)
+    mlflow.log_metric('columnas', 1)
+    
