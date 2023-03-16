317050cbe7885b22d63f4c1370e2afd18d2cf98d
Pablo Andrés Tamayo Flórez <pablo.tamayo@DATALYTICS.local>
2023-03-16 16:42:51 -0500

    se dividen los datos en 3 sets diferentes

---
+++ scripts/train_test_split.py
@@ -0,0 +1,44 @@
+from azureml.core import Run
+import argparse
+import mlflow
+from sklearn.model_selection import train_test_split
+import os
+import pandas as pd
+
+run =Run.get_context()
+#transformed_data = run.input_datasets['transformed_data']
+#transformed_df = transformed_data.to_pandas_dataframe()
+
+parser = argparse.ArgumentParser("split")
+parser.add_argument('--input_data', type=str, help='data')
+parser.add_argument("--output_train_data", type=str, help="train data directory")
+parser.add_argument("--output_test_data", type=str, help="test data directory")
+parser.add_argument("--output_val_data", type=str, help="validation data directory")
+args = parser.parse_args()
+
+transformed_data_path = args.input_data
+transformed_data_file = os.path.join(transformed_data_path, 'transformed_data.csv')
+
+def write_output(df, path, name_file):
+    '''
+    Escribe el dataset como csv en una ruta determinada
+    '''
+    os.makedirs(path, exist_ok=True)
+    df.to_csv(path+name_file, index=False, header=True)
+    mlflow.log_artifact(path+name_file)
+
+with mlflow.start_run():
+    transformed_df = pd.read_csv(transformed_data_file)
+    train_data, test_data = train_test_split(transformed_df, test_size=0.20, random_state=73)
+    train_data, val_data = train_test_split(train_data, train_size=0.80, random_state=37)
+    train_data.reset_index(inplace=True, drop=True)
+    test_data.reset_index(inplace=True, drop=True)
+    val_data.reset_index(inplace=True, drop=True)
+
+    if not( args.output_train_data is None and args.output_test_data is None):
+
+        write_output(train_data, args.output_train_data, '/train_data.csv')
+        write_output(test_data, args.output_test_data, '/test_data.csv')
+        write_output(val_data, args.output_val_data, '/val_data.csv')
+
+
