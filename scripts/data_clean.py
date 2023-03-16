2f876895e0a8c5d376ad54936253f59b1af8bb17
Pablo Andrés Tamayo Flórez <pablo.tamayo@DATALYTICS.local>
2023-03-16 16:36:05 -0500

    dejar solos los datos de los 5 aeropuertos con mas registros

--- scripts/data_clean.py
+++ scripts/data_clean.py
@@ -19,12 +19,12 @@ print("Argument 3(output cleansed taxi data path): %s" % args.output_cleanse)
 with mlflow.start_run():
 
     df = raw_data.to_pandas_dataframe().drop('Flight', axis=1, inplace=False)
+    df = df[df['Airline'].isin(df['Airline'].value_counts().head(5).to_dict().keys())].reset_index(drop=True)
     
     if not (args.output_cleanse is None):
-        mlflow.log_metric('step1', 111)
         os.makedirs(args.output_cleanse, exist_ok=True)
         print("%s created" % args.output_cleanse)
-        path = args.output_cleanse + "/processed.csv"
-        write_df = df.to_csv(path,index=False)
+        path = os.path.join(args.output_cleanse, "clean_data.csv")
+        write_df = df.to_csv(path,index=False, header=True)
         #df.to_csv('clean.csv')
         mlflow.log_artifact(path)
