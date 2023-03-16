194b82c8630c454e45bc52c6d396d3af9f8a4d04
Pablo Andrés Tamayo Flórez <pablo.tamayo@DATALYTICS.local>
2023-03-16 16:37:56 -0500

    agregar logging para debug

--- scripts/data_transform.py
+++ scripts/data_transform.py
@@ -12,19 +12,23 @@ import os
 
 run = Run.get_context()
 
-clean_data = run.input_datasets['clean_airlines_data']
+#clean_data = run.input_datasets['cleaned_data']
 
 parser = argparse.ArgumentParser("transform")
+parser.add_argument('--input_data', type=str, help='data to transform')
 parser.add_argument("--output_transform", type=str, help="transformed data directory")
 
 args = parser.parse_args()
+
+clean_data_path = args.input_data
+clean_data_file = os.path.join(clean_data_path, 'clean_data.csv')
 
 print("Argument 3(output cleansed taxi data path): %s" % args.output_transform)
 
 
 with mlflow.start_run():
-    df = clean_data.to_pandas_dataframe()
-    mlflow.log_metric('b', len(df.columns))
+    df = pd.read_csv(clean_data_file)
+    #mlflow.log_metric('b', len(df.columns))
     numeric_columns = ['Length','Time']
     category_columns = ['Airline', 'AirportFrom', 'AirportTo', 'DayOfWeek']
 
@@ -44,22 +48,20 @@ with mlflow.start_run():
     
     #split the dependent(y) and independent(df_x) features 
     df_X = df.drop('Class', axis=1, inplace=False)
-    mlflow.log_metric('a', len(df_X.columns))
+    print("holaaaaaaaaaaaaaaaaaaaa.................................................imposible que no me veasss")
     #y_labels = data['Class']
     y = df['Class']
     preprocessed_data = preprocessor.fit_transform(df_X)
     encoded_category = preprocessor.named_transformers_['cat']['onehot'].get_feature_names_out(category_columns)
-    mlflow.log_metric('p', len(encoded_category))
     labels = np.concatenate([numeric_columns, encoded_category])
     
     preprocessed_df = pd.DataFrame(data=preprocessed_data, columns=labels)
     transsformed_df = pd.concat([preprocessed_df, y], axis=1)
 
     if not (args.output_transform is None):
-        mlflow.log_metric('step_2', 222)
         os.makedirs(args.output_transform, exist_ok=True)
         print("%s created" % args.output_transform)
-        path = args.output_transform + "/processed_t.csv"
-        write_df = transsformed_df.to_csv(path)
+        path = args.output_transform + "/transformed_data.csv"
+        write_df = transsformed_df.to_csv(path, index=False, header=True)
         #transsformed_df.to_csv('transform.csv')
         mlflow.log_artifact(path)
