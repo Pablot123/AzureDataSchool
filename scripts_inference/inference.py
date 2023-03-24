fced1a8d983b6ebc028996c8a0a2e13b1e2b893e
Pablo Andrés Tamayo Flórez <pablo.tamayo@DATALYTICS.local>
2023-03-24 09:57:35 -0500

    se arregla la inferencia colocando el preprocesamiento

--- scripts_inference/inference.py
+++ scripts_inference/inference.py
@@ -1,17 +1,21 @@
 import mlflow
 from azureml.core import Workspace, Datastore, Run, Dataset
+from sklearn.metrics import balanced_accuracy_score
 import pandas as pd
+from src.utils import dataset_transform
 import argparse
 import os
 
-#parser = argparse.ArgumentParser("Inference")
 
-#parser.add_argument("--output_preds", type=str, help="validation data directory")
-#args = parser.parse_args()
+run = Run.get_context()
 
-ws = Run.get_context().experiment.workspace
+ws = run.experiment.workspace
 
-with mlflow.start_run() as run:
+raw_data_test = run.input_datasets['raw_test_data'].to_pandas_dataframe()
+
+with mlflow.start_run():
+
+    transformed_data_test = dataset_transform(test_df=raw_data_test)
 
     #load the latest version of the model
     model_name = 'airlines_model'
@@ -19,17 +23,18 @@ with mlflow.start_run() as run:
     print('loaded model')
     #mlflow.log_metric('prueba',1)
 
-    #load the test dataset
-    dataset = Dataset.get_by_name(ws, name='airlines_test_data', version='latest')
-    test_df = dataset.to_pandas_dataframe()
+    pred = model.predict(transformed_data_test.drop('Class', axis=1, inplace=False))
 
-    pred = model.predict(test_df.drop('Class', axis=1, inplace=False))
+    acc = balanced_accuracy_score(transformed_data_test['Class'], pred)
+    mlflow.log_metric('acc', acc)
 
     #guardar en un archivo plano
     pred_df = pd.DataFrame(pred, columns=['predicted_Class'])
     #output_path = os.path.join(args.output_preds, 'test_predictions.csv')
     pred_df.to_csv('test_predictions.csv', index=False, header=True)
     mlflow.log_artifact('test_predictions.csv')
+
+
 
     
 
